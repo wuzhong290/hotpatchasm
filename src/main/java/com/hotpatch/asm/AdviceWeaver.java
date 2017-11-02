67d5c7f0e57b9c0a66771281fb634a4d214aab41
YOHO01\zhong.wu <zhong.wu>
2017-11-02 18:40:53 +0800

    asm

--- src/main/java/com/hotpatch/asm/AdviceWeaver.java
+++ src/main/java/com/hotpatch/asm/AdviceWeaver.java
@@ -16,69 +16,13 @@ public class AdviceWeaver  extends ClassVisitor implements Opcodes {
     }
 
     @Override
-    public MethodVisitor visitMethod(int access, final String name, String desc, String signature, String[] exceptions) {
-        final MethodVisitor mv = super.visitMethod(access, name, desc, signature, exceptions);
-        if (isIgnore(mv, access, name, desc)) {
-            return mv;
-        }
-        logger.info("visitMethod name:"+name);
+    public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
+        MethodVisitor mv = super.visitMethod(access, name, desc, signature, exceptions);
         if (name.equals("getNumber")) {
-            logger.info("visitMethod getNumber getNumber");
             mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
             mv.visitMethodInsn(INVOKESTATIC, "java/lang/System", "currentTimeMillis", "()J");
             mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(J)V");
         }
         return mv;
-//        return new AdviceAdapter(ASM5, new JSRInlinerAdapter(mv, access, name, desc, signature, exceptions), access, name, desc){
-//            private final Type ASM_TYPE_SPY = Type.getType("Lcom/hotpatch/asm/Spy;");
-//            private final Type ASM_TYPE_METHOD = Type.getType(java.lang.reflect.Method.class);
-//            private final Method ASM_METHOD_METHOD_INVOKE = Method.getMethod("Object invoke(Object,Object[])");
-//            @Override
-//            public void visitMethodInsn(int opcode, String owner, String name, String desc, boolean itf) {
-//                logger.info("visitMethodInsn begin name:"+name);
-//                super.visitMethodInsn(opcode, owner, name, desc, itf);
-//                logger.info("visitMethodInsn end name:"+name);
-//            }
-//
-//            @Override
-//            protected void onMethodEnter() {
-//            }
-//            @Override
-//            protected void onMethodExit(int opcode) {
-//            }
-//
-//            /**
-//             * 加载通知方法
-//             */
-//            private void loadAdviceMethod() {
-//                getStatic(ASM_TYPE_SPY, "ON_ASM_METHOD", ASM_TYPE_METHOD);
-//            }
-//        };
-    }
-
-    /**
-     * 是否需要忽略
-     */
-    private boolean isIgnore(MethodVisitor mv, int access, String name, String desc) {
-        return null == mv
-                || isAbstract(access)
-                || isEquals(name, "<init>")
-                || isEquals(name, "<clinit>");
-    }
-    /**
-     * 是否抽象属性
-     */
-    private boolean isAbstract(int access) {
-        return (ACC_ABSTRACT & access) == ACC_ABSTRACT;
-    }
-
-    public static <E> boolean isEquals(E src, E target) {
-
-        return null == src
-                && null == target
-                || null != src
-                && null != target
-                && src.equals(target);
-
     }
 }
--- src/main/java/com/hotpatch/asm/Enhancer.java
+++ src/main/java/com/hotpatch/asm/Enhancer.java
@@ -13,9 +13,6 @@ import java.lang.instrument.IllegalClassFormatException;
 import java.lang.reflect.InvocationTargetException;
 import java.security.ProtectionDomain;
 
-import static org.objectweb.asm.ClassWriter.COMPUTE_FRAMES;
-import static org.objectweb.asm.ClassWriter.COMPUTE_MAXS;
-
 /**
  * Created by wuzhong on 2017/11/2.
  */
@@ -38,7 +35,7 @@ public class Enhancer extends ClassLoader implements ClassFileTransformer {
             }
             cr = new ClassReader(array);
             // 字节码增强
-            final ClassWriter cw = new ClassWriter(cr, COMPUTE_FRAMES | COMPUTE_MAXS);
+            ClassWriter cw=new ClassWriter(ClassWriter.COMPUTE_MAXS);
             // 生成增强字节码
             cr.accept(new AdviceWeaver(cw), 0);//EXPAND_FRAMES
             byte[] enhanceClassByteArray = cw.toByteArray();
